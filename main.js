c1f4721801e1b5596111685cef6b6641de5079c8
noemargui <noe.marguillard@essec.edu>
2018-12-27 09:07:08 +0100

    Dernieres modifs

---
+++ main.js
@@ -0,0 +1,32 @@
+function changefunction()
+{
+	document.body.style.backgroundColor = "magenta";
+	document.getElementById('h1').style.color = "green";
+	document.getElementById('h2a').style.color = "green";
+	document.getElementById('h2b').style.color = "green";
+	document.getElementById('h2c').style.color = "green";
+	document.getElementById('h2d').style.color = "green";
+	document.getElementById('h2e').style.color = "green";
+	document.getElementById('h2f').style.color = "green";
+	document.getElementById('h3a').style.color = "green";
+	document.getElementById('h3b').style.color = "green";
+	document.body.style.color = "blue";
+	document.getElementById('h1').style.fontFamily = "Comic Sans MS";
+	document.getElementById('h2a').style.fontFamily = "Comic Sans MS";
+	document.getElementById('h2b').style.fontFamily = "Comic Sans MS";
+	document.getElementById('h2c').style.fontFamily = "Comic Sans MS";
+	document.getElementById('h2d').style.fontFamily = "Comic Sans MS";
+	document.getElementById('h2e').style.fontFamily = "Comic Sans MS";
+	document.getElementById('h2f').style.fontFamily = "Comic Sans MS";
+	document.getElementById('h3a').style.fontFamily = "Comic Sans MS";
+	document.getElementById('h3b').style.fontFamily = "Comic Sans MS";
+	document.body.style.fontFamily = "Papyrus";
+}
+
+setTimeout(function()
+{
+	if(confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"))
+	{
+			location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+	}
+}, 10000)
